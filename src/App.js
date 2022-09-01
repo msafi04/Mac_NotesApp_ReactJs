a8482b01ce12dd8060c0844b5ab999ce5cc2fdda
msafi04 <mohamedsafi@Mohameds-MacBook-Pro.local>
2022-09-01 13:40:35 +0800

    Added Toggle Switch

--- src/App.js
+++ src/App.js
@@ -5,24 +5,41 @@ import {
   Route
 } from "react-router-dom";
 
+// to create light/dark mode
+import { createContext, useState } from "react";
+import ReactSwitch from "react-switch";
+
 import './App.css';
 import Header from './components/Header';
 import NotesListPage from './pages/NotesListPage';
 import NotePage from './pages/NotePage';
 
+export const ThemeContext = createContext(null);
+
 function App() {
+
+  const [theme, setTheme] = useState("dark")
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light")); // logic to set: if light then dark vice versa
+  }
+
   return (
-    <Router>
-      <div className="container dark">
-        <div className="app">
-          <Header />
-          <Routes>
-            <Route path='/' exact element={<NotesListPage />} />
-            <Route path='/note/:id' element={<NotePage />} />
-          </Routes>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <Router>
+        <div className="container">
+          <div className="app" id={ theme }>
+            <Header />
+            <div className="switch">
+              <ReactSwitch onChange={ toggleTheme } checked={theme === "dark"}/>
+            </div>
+            <Routes>
+              <Route path='/' exact element={<NotesListPage />} />
+              <Route path='/note/:id' element={<NotePage />} />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </ThemeContext.Provider>
   );
 }
 
